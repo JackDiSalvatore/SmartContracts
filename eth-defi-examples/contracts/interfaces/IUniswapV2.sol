4836ffe19f352682aac1b281ae1f5119f3bd0d66
JackDiSalvatore <jackp.disalvatore@gmail.com>
2025-10-25 16:02:07 -0500

    moving defi examples
    Signed-off-by: JackDiSalvatore <jackp.disalvatore@gmail.com>

---
+++ eth-defi-examples/contracts/interfaces/IUniswapV2.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// https://uniswap.org/docs/v2/smart-contracts
+
+interface IUniswapV2Router {
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (
+        uint amountA,
+        uint amountB,
+        uint liquidity
+    );
+
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB);
+}
+
+interface IUniswapV2Factory {
+    function getPair(address token0, address token1) external view returns (address);
+}
