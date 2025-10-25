18e932128efaf1a8364546815d02db330f2a6ba7
JackDiSalvatore <jackp.disalvatore@gmail.com>
2025-10-25 15:57:51 -0500

    moving eth erc20
    Signed-off-by: JackDiSalvatore <jackp.disalvatore@gmail.com>

---
+++ eth-erc20token/test/helpers.js
@@ -0,0 +1,13 @@
+exports.toBN = (value) => {
+  return web3.utils.toBN(value)
+}
+
+exports.ether = (value) => {
+  return web3.utils.toBN(
+    // converts Ether value to Wei
+    web3.utils.toWei(value.toString(), 'ether')
+  )
+}
+
+exports.tokens = (value) => ether(value)
+
